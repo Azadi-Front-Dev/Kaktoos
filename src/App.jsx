beaf6947770593ab213a3c476558900098d51c27
Ali <ali.azadi.dev@gmail.com>
2026-06-12 22:40:22 +0330

    feat : configure react-router-dom routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { useRoutes } from "react-router-dom";
+
+const routes = [{ path: "/", element: <></> }];
+
 function App() {
+  const router = useRoutes(routes);
   return (
     <>
       <div className="bg-gray-300">hello</div>
+      {router}
     </>
   );
 }
